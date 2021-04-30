6e478bfd46c1df62330c6f46d1a9648e0bf88984
Joyce Siqueira <joycitta@gmail.com>
2021-04-29 21:44:16 -0300

    atualização - remover item

--- alocacao.cpp
+++ alocacao.cpp
@@ -3,9 +3,9 @@
 
 #include<stdio.h>
 #include<stdlib.h>
-typedef struct lista lista;//criação do tipo "lista"
-struct lista { //criei a estrutuda de cada nó da lista
-	int codigo; //pode ter quantas variáveis forem necessárias
+typedef struct lista lista;//criaÃ§Ã£o do tipo "lista"
+struct lista { //criei a estrutuda de cada nÃ³ da lista
+	int codigo; //pode ter quantas variÃ¡veis forem necessÃ¡rias
 	lista *proximo; //obrigatoriamente o ponteiro
 };
 lista* inicializar();
@@ -24,11 +24,11 @@ lista* inicializar(){
 	return NULL;
 }
 lista* inserir(lista* listaCodigos){
-	lista* novo = (lista*) malloc(sizeof(listaCodigos));//alocação de espaço
+	lista* novo = (lista*) malloc(sizeof(listaCodigos));//alocaÃ§Ã£o de espaÃ§o
 	printf("Digite o codigo: ");
 	scanf("%d",&novo->codigo); //solicitando os dados
 	fflush(stdin);
-	novo->proximo=listaCodigos; //preenchendo o ponteiro com a proxima posição
+	novo->proximo=listaCodigos; //preenchendo o ponteiro com a proxima posiÃ§Ã£o
 	return novo;	
 }
 
@@ -36,6 +36,36 @@ void relatorio (lista* listaCodigos){
 	for (listaCodigos;listaCodigos!=NULL;listaCodigos=listaCodigos->proximo){
 		printf("Codigo: %d\n", listaCodigos->codigo);	
 	}
+}
+
+lista* remover (lista* listaCodigos){
+	lista* anterior; //ponteiro que guarda a posiÃ§Ã£o do elemento anterior
+	lista* auxiliar; //lista auxiliar para buscar na listaCodigos
+	
+	int codigoBusca;
+	printf("Digite o codigo do cadastro");
+	scanf("%d", &codigoBusca);
+	
+	for(anterior=NULL, auxiliar=listaCodigos;auxiliar!=NULL && auxiliar->codigo!=codigoBusca; anterior=auxiliar, auxiliar=auxiliar->proximo){
+	}
+	
+	if (auxiliar==NULL){
+		printf("codigo nao cadastrado - lista vazia");
+		return listaCodigos;
+	}
+	
+	if (anterior==NULL){
+		listaCodigos = auxiliar->proximo;
+		free(auxiliar);
+	}
+	else {
+		anterior->proximo = auxiliar->proximo;
+		free(auxiliar);
+	}
+	printf("Codigo removido!");
+	
+	return listaCodigos;
+	
 }
 
 
